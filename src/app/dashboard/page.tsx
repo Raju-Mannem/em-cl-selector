cded4e2f7f05309f0ec2866b70b922a102548ec4
Raju Mannem <rajumannem71@gmail.com>
2025-03-15 15:33:25 +0530

    new instructions added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import College from "@/components/College";
 
 const Dashboard = () => {
   return (
-    <main className="h-full w-full px-4 py-2">
+    <main className="h-full w-full px-2 py-2">
 		<College/>
     </main>
   )
