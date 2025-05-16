a53365a2eea09c80b5d3df6e0f5e7a8e927c91bb
Raju Mannem <rajumannem71@gmail.com>
2025-05-16 12:46:25 +0530

    ts cutoff2025 component v1

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,36 @@
+"use client";
+import { useState } from "react";
 import College from "@/components/College";
+import Cutoff2025 from "@/components/Cutoff2025";
 
 const Dashboard = () => {
+  const [selectType, setSelectType] = useState<string>("");
+  const handleSelect = (option: string) => {
+    setSelectType(option);
+  };
   return (
     <main className="h-full w-full px-2 py-2">
-		<College/>
+      <section className="flex justify-end items-center px-12 py-4 my-4 text-[8px] sm:text-sm font-sans bg-indigo-200">
+        <article className="px-2 mx-8 bg-stone-50 rounded-sm">
+          <button
+            onClick={() => handleSelect("colleges")}
+            className="focus:outline-0"
+          >
+            College
+          </button>
+        </article>
+        <article className="px-2 mx-8 bg-stone-50 rounded-sm">
+          <button
+            onClick={() => handleSelect("cutoff")}
+            className="focus:outline-0"
+          >
+            Cutoff 2025
+          </button>
+        </article>
+      </section>
+      {selectType == "colleges" ? <College /> : <Cutoff2025 />}
     </main>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
